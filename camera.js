d90c7b8bc6562a8cfdb391051d6a1ad9cd1c37ee
MrTuyen123 <150044843+MrTuyen123@users.noreply.github.com>
2026-09-22 13:13:38 +0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,21 +1,54 @@
 const video = document.getElementById("video");
-navigator.mediaDevices
-.getUserMedia({
-    video:{
-        facingMode:"user"
-    }
+
+Promise.all([
+ faceapi.nets.tinyFaceDetector.loadFromUri(
+ "https://justadudewhohacks.github.io/face-api.js/models"
+ )
+])
+.then(startCamera);
+
+
+function startCamera(){
+
+navigator.mediaDevices.getUserMedia({
+ video:{facingMode:"user"}
 })
 .then(stream=>{
 
-    video.srcObject = stream;
+video.srcObject = stream;
 
-    document.getElementById("status").innerHTML =
-    "✅ Camera đang hoạt động";
+document.getElementById("status").innerHTML =
+"✅ Camera hoạt động - AI đang khởi động";
 
 })
 .catch(error=>{
 
-    document.getElementById("status").innerHTML =
-    "❌ Lỗi camera: " + error;
+document.getElementById("status").innerHTML =
+"❌ Lỗi camera: "+error;
+
+});
+
+}
+
+
+video.addEventListener("play",()=>{
+
+setInterval(async()=>{
+
+const result =
+await faceapi.detectSingleFace(
+video,
+new faceapi.TinyFaceDetectorOptions()
+);
+
+
+if(result){
+
+document.getElementById("status").innerHTML =
+"😀 Đã phát hiện khuôn mặt";
+
+}
+
+},1000);
 
 });
