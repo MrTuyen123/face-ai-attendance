f1012056326170e77391a72bd20fa5e4db874f1e
MrTuyen123 <150044843+MrTuyen123@users.noreply.github.com>
2026-09-22 12:30:37 +0700

    Create camera.js

---
+++ camera.js
@@ -0,0 +1,22 @@
+const video = document.getElementById("video");
+
+navigator.mediaDevices
+.getUserMedia({
+    video:{
+        facingMode:"user"
+    }
+})
+.then(stream=>{
+
+    video.srcObject = stream;
+
+    document.getElementById("status").innerHTML =
+    "✅ Camera đang hoạt động";
+
+})
+.catch(error=>{
+
+    document.getElementById("status").innerHTML =
+    "❌ Lỗi camera: " + error;
+
+});
